5da9780ed423df297f0660a96e7e1a9ecc1de21a
RakshithMurthy <35984675+RakshithMurthy@users.noreply.github.com>
2019-09-13 12:22:37 +0530

    Create F1b.py

---
+++ F1b.py
@@ -0,0 +1,8 @@
+                                                tuple
+def tuplefun():
+    showroom = ("audi","benz","kia")
+    print("number of cars in the  showroom is",len(showroom))
+
+    showroom2 = ("maruthi","mg",showroom)
+    print(" the number of cars in",showroom2*3)
+tuplefun()
